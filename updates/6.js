c986309508b8ca4a736339a62b5e840731d9e4d9
Louis Thiery <me@louisthiery.com>
2014-03-04 15:27:23 -0500

    Update 6.js
    update try 2

--- updates/6.js
+++ updates/6.js
@@ -6,7 +6,10 @@ module.exports = function(callback) {
   console.log("running 5 \n")
   cmd += 'cd /root/;'
   cmd += 'rm -r GroundHive;'
-  cmd += 'curl -L https://github.com/apitronics/GroundHive/archive/1.0-beta3.tar.gz | tar -zxf GroundHive;"
+  cmd += 'wget https://github.com/apitronics/GroundHive/archive/1.0-beta3.tar.gz;"
+  cmd += 'tar xf 1.0-beta3.tar.gz;'
+  cmd += 'mv GroundHive-1.0-beta3/ GroundHive;'
+  cmd += 'rm 1.0-beta3.tar.gz'
   exec(cmd, function() {
     callback()
   })
