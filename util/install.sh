20239b0e188b4668896e8eae24fef75f88b6119e
rjsteinert <rj@rjsteinert.com>
2014-03-10 13:14:56 -0400

    ground hive -> hive

--- util/install.sh
+++ util/install.sh
@@ -1,3 +1,3 @@
 
-cp ./systemd/update-ground-hive-updater.service /etc/systemd/system/multi-user.target.wants/
-cp ./systemd/ground-hive-updater.service /etc/systemd/system/multi-user.target.wants/
+cp ./systemd/update-hive-updater.service /etc/systemd/system/multi-user.target.wants/
+cp ./systemd/hive-updater.service /etc/systemd/system/multi-user.target.wants/
