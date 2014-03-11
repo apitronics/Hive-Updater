731286ebf2f83862b8185c8924a069f47369547a
Louis Thiery <me@louisthiery.com>
2014-03-11 12:51:04 -0400

    Update install.sh

--- util/install.sh
+++ util/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cp ./systemd/update-hive-updater.service /etc/systemd/system/multi-user.target.wants/
-cp ./systemd/hive-updater.service /etc/systemd/system/multi-user.target.wants/
+ln ./systemd/hive-updater.service /etc/systemd/system/multi-user.target.wants/
