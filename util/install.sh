faaa9f1c3b38d574de771f8b156239b8819149bc
Louis Thiery <me@louisthiery.com>
2014-03-11 15:57:43 -0400

    Update install.sh

--- util/install.sh
+++ util/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp /root/.Hive-Updater/util/Settings.default.js /root/.Hive-Updater/Settings.js
-ln /root/.Hive-Updater/util//systemd/hive-updater.service /etc/systemd/system/multi-user.target.wants/
+ln -s /root/.Hive-Updater/util//systemd/hive-updater.service /etc/systemd/system/multi-user.target.wants/
