d5218d9b606985b486dbc2653d9fe84c2cf1c44c
rjsteinert <rj@rjsteinert.com>
2014-03-24 17:08:58 -0400

    Updates

--- server.js
+++ server.js
@@ -77,3 +77,41 @@ server.get('/*', function(req, res){
 
 server.listen(124);
 console.log('Hive-Updater listening on port 124')
+
+
+setTimeout(function() {
+ 
+  var ev = new Backbone.Model()
+  var history
+  var updates
+
+  // Get the history file
+  ev.on('0', function() {
+    fs.readFile(Settings.path + '/data/history.json','utf8', function(err, data) {
+      if (err) {
+        // no history yet, just create a blank history
+        history = []
+      }
+      else {
+        history = JSON.parse(data)
+      }
+      ev.trigger('1')
+    })
+  })
+
+  // Get the updates file
+  ev.on('1', function() {
+    fs.readFile(Settings.path + '/data/updates.json','utf8', function(err, data) {
+      if (err) throw err
+      updates = JSON.parse(data)
+      ev.trigger('2')
+    })
+  })
+
+  // Compile a list of todo's
+  ev.on('2', function() {
+    var todo = _.difference(_.keys(updates), history)
+    console.log(JSON.stringify(todo))
+  })
+
+}, 30000)
