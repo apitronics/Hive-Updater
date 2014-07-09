9478885eb2fc0d829c8d3241c601ed6af5fc7c5e
Hao Chen <detectx@gmail.com>
2014-07-09 16:40:32 -0400

    Add update for cloud integration

--- updates/1_0_2.js
+++ updates/1_0_2.js
@@ -5,8 +5,30 @@ function puts(error, stdout, stderr) { sys.puts(stdout); }
 module.exports = function(callback) {
   console.log("installing setuptools\n");
   cmd += 'wget https://bootstrap.pypa.io/ez_setup.py -O - | python';
+
   console.log("installing pytz\n");
   cmd += 'easy_install --upgrade pytz';
+
+  console.log("updating hive\n");
+  var hive_version = '1.1-beta1';
+  cmd += 'cd /root/;';
+  cmd += 'rm -rf GroundHive;';
+  cmd += 'rm -rf Hive;';
+  cmd += 'wget https://github.com/apitronics/Hive/archive/' + hive_version + '.tar.gz;';
+  cmd += 'tar xf '+ hive_version + '.tar.gz;';
+  cmd += 'mv Hive-' + hive_version + '/ Hive;';
+  cmd += 'rm -f ' + hive_version + '.tar.gz;';
+  cmd += 'cd Hive;';
+  cmd += 'npm install;';
+  cmd += 'cp util/Settings.default.js ./Settings.js;';
+  cmd += './start.sh';
+
+  console.log("updating cron jobs\n");
+  cmd += 'systemctl stop cronie';
+  cmd += 'rm /var/spool/cron/root';
+  cmd += 'crontab /root/Hive/util/var/spool/cron/root';
+  cmd += 'systemctl start cronie';
+
   exec(cmd, function() {
     callback();
   });
