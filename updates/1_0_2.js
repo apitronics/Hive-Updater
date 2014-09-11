e8678581563f769ed27ca501d2b16ad071eb6b4b
Hao Chen <detectx@gmail.com>
2014-09-11 13:48:46 -0400

    Fix update #2

--- updates/1_0_2.js
+++ updates/1_0_2.js
@@ -4,36 +4,39 @@ function puts(error, stdout, stderr) { sys.puts(stdout); }
 
 module.exports = function(callback) {
   console.log("installing setuptools\n");
-  cmd += 'wget https://bootstrap.pypa.io/ez_setup.py -O - | python;';
+  cmd += 'wget https://bootstrap.pypa.io/ez_setup.py -O - | python && ';
+  cmd += 'rm -f setuptools-5.7.zip && ';
 
   console.log("installing pytz\n");
-  cmd += 'easy_install --upgrade pytz;';
+  cmd += 'easy_install --upgrade pytz; ';
 
   console.log("installing six\n");
-  cmd += 'easy_install --upgrade six;';
+  cmd += 'easy_install --upgrade six; ';
 
   console.log("updating sensor defs\n");
-  cmd += 'curl -H "Content-Type: application/json" -X POST http://localhost:5984/_replicate -d \'{"source": "http://db.apitronics.com:5984/config", "target": "config"}\';';
+  cmd += 'curl -H "Content-Type: application/json" -X POST http://localhost:5984/_replicate -d \'{"source": "http://db.apitronics.com:5984/config", "target": "config"}\'; ';
 
   console.log("updating hive\n");
   var hive_version = '1.1-beta1';
-  cmd += 'cd /root/;';
-  cmd += 'rm -rf GroundHive;';
-  cmd += 'rm -rf Hive;';
-  cmd += 'wget https://github.com/apitronics/Hive/archive/' + hive_version + '.tar.gz;';
-  cmd += 'tar xf '+ hive_version + '.tar.gz;';
-  cmd += 'mv Hive-' + hive_version + '/ Hive;';
-  cmd += 'rm -f ' + hive_version + '.tar.gz;';
-  cmd += 'cd Hive;';
-  cmd += 'npm install;';
-  cmd += 'cp util/Settings.default.js ./Settings.js;';
-  cmd += './start.sh;';
+  cmd += '/root/GroundHive/stop.sh; ';
+  cmd += '/root/Hive/stop.sh; ';
+  cmd += 'cd /root && ';
+  cmd += 'rm -rf GroundHive && ';
+  cmd += 'rm -rf Hive && ';
+  cmd += 'wget https://github.com/apitronics/Hive/archive/' + hive_version + '.tar.gz -O /root/Hive.tar.gz && ';
+  cmd += 'tar xf Hive.tar.gz && ';
+  cmd += 'mv Hive-' + hive_version + ' Hive && ';
+  cmd += 'rm -f /root/Hive.tar.gz && ';
+  cmd += 'cd /root/Hive && ';
+  cmd += 'npm install && ';
+  cmd += 'cp util/Settings.default.js ./Settings.js && ';
+  cmd += './start.sh && ';
 
   console.log("updating cron jobs\n");
-  cmd += 'systemctl stop cronie;';
-  cmd += 'rm /var/spool/cron/root;';
-  cmd += 'crontab /root/Hive/util/var/spool/cron/root;';
-  cmd += 'systemctl start cronie;';
+  cmd += 'systemctl stop cronie && ';
+  cmd += 'rm /var/spool/cron/root && ';
+  cmd += 'crontab /root/Hive/util/var/spool/cron/root && ';
+  cmd += 'systemctl start cronie';
 
   exec(cmd, function() {
     callback();
